f337c5fd580e35c3e8894b823bf3a700f9250cc1
EricLottman <rick821044@gmail.com>
2024-09-23 20:10:57 -0400

    Added Integration for IMU

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Integrator.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Integrator {
+
+    private double velocityX, velocityY;
+    private double positionX, positionY;
+    private double lastAccelerationX, lastAccelerationY;
+    private double lastHeading;
+    private double alpha = 0.8; // Smoothing factor for low-pass filter (0 < alpha < 1)
+
+    public Integrator() {
+        this.velocityX = 0.0;
+        this.velocityY = 0.0;
+        this.positionX = 0.0;
+        this.positionY = 0.0;
+        this.lastAccelerationX = 0.0;
+        this.lastAccelerationY = 0.0;
+        this.lastHeading = 0.0;
+    }
+
+    /**
+     * Updates the integrated values based on the current acceleration, heading, and timestamp.
+     * @param accelerationX The current acceleration value in x direction in meters per second squared (m/s^2).
+     * @param accelerationY The current acceleration value in y direction in meters per second squared (m/s^2).
+     * @param heading The current heading in degrees.
+     * @param seconds The time elapsed in seconds since the last update.
+     */
+    public synchronized void update(double accelerationX, double accelerationY, double heading, double seconds) {
+
+        // apply low pass filter to increase accuracy
+        accelerationX = applyLowPassFilter(accelerationX, lastAccelerationX);
+        accelerationY = applyLowPassFilter(accelerationY, lastAccelerationY);
+
+        // get change in heading
+        double deltaHeading = heading - lastHeading;
+
+        // average acceleration to simulate trapezoidal rule integration approximation
+        double avgAccelerationX = (lastAccelerationX + accelerationX) / 2.0;
+        double avgAccelerationY = (lastAccelerationY + accelerationY) / 2.0;
+
+        // integrate velocity and position with respect to the average acceleration
+        // delta x is seconds, height of rectangle is acceleration
+        velocityX += avgAccelerationX * seconds;
+        velocityY += avgAccelerationY * seconds;
+        // this feels easier than making you have to call an Integrator within an Integrator, less objects but feels less cool
+        positionX += velocityX * seconds;
+        positionY += velocityY * seconds;
+        // it is rn that i learned that theres such thing as absement, remember that for the future, as well as the fact that theres anti (abs-) versions of things and some people go to abserk
+
+        // adjust position for heading change (aka be capable of turning without breaking algorithm)
+        double radians = Math.toRadians((heading + lastHeading) / 2.0); //originally used delta heading, now we use the average heading to account for turning better
+        double cosHeading = Math.cos(radians);
+        double sinHeading = Math.sin(radians);
+
+        double newPositionX = positionX * cosHeading - positionY * sinHeading;
+        double newPositionY = positionX * sinHeading + positionY * cosHeading;
+
+        positionX = newPositionX;
+        positionY = newPositionY;
+
+        // update states
+        lastAccelerationX = accelerationX;
+        lastAccelerationY = accelerationY;
+        lastHeading = heading;
+    }
+
+    private double applyLowPassFilter(double current, double previous) {
+        return alpha * previous + (1 - alpha) * current;
+    }
+
+    // getter time
+    // 39.3701 is the conversion factor to multiply by to get inches from meters, since I operate in freedom units *cue eagle screech* and BNO055IMU.AccelUnit doesnt have a freedom unit option, trust me I checked
+    public synchronized double getVelocityX() {
+        return velocityX * 39.3701;
+    }
+
+    public synchronized double getVelocityY() {
+        return velocityY * 39.3701;
+    }
+
+    public synchronized double getPositionX() {
+        return positionX*39.3701;
+    }
+
+    public synchronized double getPositionY() {
+        return positionY*39.3701;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleOpModeWithIntegrator.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@Autonomous
+public class SampleOpModeWithIntegrator extends LinearOpMode {
+    private BNO055IMU imu;
+    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+    private Integrator integrator = new Integrator();
+    private Thread integratorThread;
+    private volatile boolean running = true; // Use volatile to ensure visibility across threads
+
+    public void runOpMode() {
+        // hardware map
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        // setup imu parameters
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+//        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu.initialize(parameters);
+
+        integratorThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ElapsedTime timer = new ElapsedTime();
+                double lastTime = 0;
+                while (running) {
+                    // get current data
+                    Acceleration acceleration = imu.getLinearAcceleration();
+                    Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                    double xA = acceleration.xAccel;
+                    double yA = acceleration.yAccel;
+                    double heading = AngleUnit.DEGREES.normalizeDegrees(angles.firstAngle);
+
+                    double currentTime = timer.seconds();
+                    double timeDelta = currentTime - lastTime; // less taxing then a timer.reset();
+                    lastTime = currentTime; // update state for loop
+
+                    // update the integrator
+                    integrator.update(xA, yA, heading, timeDelta);
+                    timer.reset();
+
+                    // sleep to control stress on the cpu, as well as accuracy
+                    try {
+                        Thread.sleep(20); // bigger to stress cpu less but will make result less accurate, vice versa with smaller values
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        });
+        integratorThread.start();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+//            telemetry.addData("Velocity X: ", integrator.getVelocityX());
+//            telemetry.addData("Velocity Y: ", integrator.getVelocityY());
+            telemetry.addData("Position X: ", integrator.getPositionX());
+            telemetry.addData("Position Y: ", integrator.getPositionY());
+            telemetry.update();
+        }
+        // stop the integrator when opMode is no longer active
+        running = false;
+        try {
+            integratorThread.join(); // Wait for the thread to finish
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
