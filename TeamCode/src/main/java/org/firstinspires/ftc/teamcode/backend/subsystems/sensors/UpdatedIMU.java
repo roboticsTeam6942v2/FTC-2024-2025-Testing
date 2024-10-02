75ea26b49c847dce0666a43384cd208cb7f8501e
EricLottman <rick821044@gmail.com>
2024-10-02 19:34:19 -0400

    Made new IMU class using the BNO055IMU class because first is moving away from the other IMU class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/subsystems/sensors/UpdatedIMU.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode.backend.subsystems.sensors;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.backend.libraries.subsystem;
+
+public class UpdatedIMU extends subsystem {
+    private Orientation globalAngles = new Orientation();
+    private double referenceAngle;
+    private BNO055IMU imu;
+    private BNO055IMU.Parameters parameters;
+
+    public UpdatedIMU(String name, @NonNull HardwareMap hwMap) {
+        this(name, hwMap, null);
+    }
+    public UpdatedIMU(String name, @NonNull HardwareMap hwMap, BNO055IMU.Parameters parameters) {
+        imu = hwMap.get(BNO055IMU.class, name);
+        if (parameters!=null) {
+            this.parameters = parameters;
+        } else {
+            this.parameters = new BNO055IMU.Parameters();
+            this.parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+            this.parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+            this.parameters.loggingEnabled = false;
+//        this.parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+            imu.initialize(this.parameters);
+        }
+        referenceAngle = 0;
+    }
+
+    /**
+     * Resets the global angle of the IMU
+     */
+    public void resetAngle() {
+        globalAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        referenceAngle = 0;
+    }
+    /**
+     * Returns the current angle of the robot
+     * @return referenceAngle Current angle
+     */
+    public double getAngle() {
+        return this.getAngle(AngleUnit.DEGREES);
+    }
+    /**
+     * Returns the current angle of the robot
+     * @param angleunit Unit you want angle to be in
+     * @return referenceAngle Current angle
+     */
+    public double getAngle(AngleUnit angleunit) {
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, angleunit);
+        referenceAngle += AngleUnit.DEGREES.normalizeDegrees(angles.firstAngle);
+        globalAngles = angles;
+        return referenceAngle;
+    }
+    /**
+     * Returns the global angle of the robot
+     * @return globalAngle the global angle
+     */
+    public double getGlobalAngle(AngleUnit angleunit) {
+        return globalAngles.firstAngle;
+    }
+    public Acceleration getAcceleration () {
+        return imu.getLinearAcceleration();
+    }
+}
