a1061e9f2bec42f4ca96c1db78e6b2d05d08f952
EricLottman <rick821044@gmail.com>
2024-10-02 20:08:45 -0400

    Hi tim

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Basic.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.backend.subsystems.actuators.base.Motor;
+import org.firstinspires.ftc.teamcode.backend.subsystems.actuators.drivetrains.Mecanum;
+
+public class Basic extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        Mecanum drivetrain = new Mecanum(new Motor[]{new Motor("backLeft", hardwareMap, "reverse"), new Motor("backRight", hardwareMap), new Motor("frontLeft", hardwareMap, "reverse"), new Motor("frontRight", hardwareMap)});
+        waitForStart();
+
+        while (opModeIsActive()) {
+            drivetrain.teleOpDrive(-gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
+        }
+    }
+}
