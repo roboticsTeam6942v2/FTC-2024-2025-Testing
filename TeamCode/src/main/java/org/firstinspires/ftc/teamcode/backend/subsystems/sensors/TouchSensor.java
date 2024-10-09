e82012fb7fa368f5a6ce6472225625e00bb9ddd8
EricLottman <rick821044@gmail.com>
2024-10-09 19:08:09 -0400

    Added TouchSensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/subsystems/sensors/TouchSensor.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.backend.subsystems.sensors;
+
+import androidx.annotation.NonNull;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.backend.libraries.subsystem;
+
+public class TouchSensor extends subsystem {
+
+    private com.qualcomm.robotcore.hardware.TouchSensor touchSensor;
+    private Telemetry telemetry;
+
+    /**
+     * TouchSensor Object
+     *
+     * @param name      Name of the TouchSensor in the phone
+     * @param hwMap     HardwareMap object from OpMode
+     * @param telemetry Telemetry object from OpMode
+     */
+    public TouchSensor(String name, @NonNull HardwareMap hwMap, Telemetry telemetry) {
+        touchSensor = hwMap.get(com.qualcomm.robotcore.hardware.TouchSensor.class, name);
+        this.telemetry = telemetry;
+    }
+
+    /**
+     * Returns the value on whether or not the sensor is pressed
+     *
+     * @return Boolean, true if pressed
+     */
+    public boolean isPressed() {
+        return touchSensor.isPressed();
+    }
+
+    /**
+     * Forces a loop until we hit an object with the sensor
+     */
+    public void untilHitObject() {
+        while (true) {
+            if (isPressed())
+                break;
+        }
+    }
+
+}
