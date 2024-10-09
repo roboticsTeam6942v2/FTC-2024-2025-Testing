0d4cc1b7fe303f0d345bbb641ac92bcea0f0e1b0
EricLottman <rick821044@gmail.com>
2024-10-09 19:09:22 -0400

    Another tiny pretty adjustment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/libraries/subsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/libraries/subsystem.java
@@ -4,11 +4,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class subsystem {
     HardwareMap hwMap;
+
     // hardware mapping must be done in run op mode so this allows us to easier control our objects
-    public subsystem(){
+    public subsystem() {
         hwMap = null;
     }
-    public subsystem(HardwareMap hardwareMap){
+
+    public subsystem(HardwareMap hardwareMap) {
         hwMap = hardwareMap;
     }
 }
