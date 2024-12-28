7dabe22d1433c4d719743d84fef6e98c5e0e5299
EricLottman <rick821044@gmail.com>
2024-12-28 10:38:48 -0500

    Computer keeps hating me so I save rn

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/winterBreak/Constants.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode.opmodes.winterBreak;
+
+import static java.lang.Math.PI;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import java.util.HashMap;
+
+public class Constants {
+
+    final static double SHOULDER_BOOSTER = 8, ELBOW_BOOSTER = 8, FINGER_STEP = .05, WRIST_STEP = .05;
+
+    public enum RobotPositions {
+        INITIAL, HOVERING_TO_GRAB, GRABBING, LOW_BASKET, HIGH_BASKET
+    }
+
+    public static DcMotorSimple.Direction
+            footDirection = DcMotorSimple.Direction.FORWARD,
+            liftRopeDirection = DcMotorSimple.Direction.FORWARD,
+            liftChainDirection = DcMotorSimple.Direction.FORWARD,
+            elbowDirection = DcMotorSimple.Direction.FORWARD;
+
+    public static Integer
+            elbowExtend = null,
+            elbowRetract = null,
+            liftLow = null,
+            liftHover = null,
+            liftMid = null,
+            liftHigh = null,
+            footDown = null;
+
+    public static Double
+            wristGrab = null,
+            wristPlace = null,
+            fingersOpen = null,
+            fingersClosed = null,
+            shoulderUp = null,
+            shoulderDown = null;
+
+    public static HashMap<RobotPositions, RobotPositionsData> positions = new HashMap<>();
+
+    static {
+        // the format is positions.put(RobotPositions.INSERT_NAME, new MotorPositions(INSERT_SHOULDER_POSITION, INSERT_ELBOW_POSITION));
+        // INITIAL position have been filled in for you as a sample
+        // please fill in HOVERING_TO_GRAB, GRABBING, LOW_BASKET, and HIGH_BASKET
+        // if you want to make new ones be my guest, youll have to add the name of the position to line 13 then add a new positions.put()
+        positions.put(RobotPositions.INITIAL, new RobotPositionsData(0, null, null));
+        positions.put(RobotPositions.HOVERING_TO_GRAB, new RobotPositionsData(liftHover, wristGrab, shoulderDown));
+
+    }
+
+
+    public static class RobotPositionsData {
+        Integer lift;
+        Double wrist, shoulder;
+
+        private RobotPositionsData(Integer lift, Double wrist, Double shoulder) {
+            this.lift = lift;
+            this.wrist = wrist;
+            this.shoulder = shoulder;
+        }
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/winterBreak/FindLimits.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/winterBreak/FindLimits.java
@@ -7,6 +7,222 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class FindLimits {
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+@TeleOp
+public class FindLimits extends LinearOpMode {
+
+    DcMotorEx foot, liftRope, liftChain, elbow;
+    Servo fingers, wrist, shoulder;
+
+    int position = 1;
+
+    String
+        spacer = "   ",
+        selector = " > ";
+
+    Gamepad last = new Gamepad();
+    double fingerPosition = 0, wristPosition = 0, shoulderPosition = 0;
+
+    Mode currentMode = Mode.DIRECTIONAL;
+
+    enum Mode {
+        DIRECTIONAL, MANUAL, FREEZE_MOTOR
+    }
+
+    @Override
+    public void runOpMode() {
+
+        // basic prepwork
+        fingers = hardwareMap.get(Servo.class, "fingers");
+        fingers.setPosition(fingerPosition);
+        wrist = hardwareMap.get(Servo.class, "wrist");
+        wrist.setPosition(wristPosition);
+        shoulder = hardwareMap.get(Servo.class, "shoulder");
+        shoulder.setPosition(shoulderPosition);
+        foot = hardwareMap.get(DcMotorEx.class, "foot");
+        foot.setDirection(Constants.footDirection);
+        liftRope = hardwareMap.get(DcMotorEx.class, "liftRope");
+        liftRope.setDirection(Constants.liftRopeDirection);
+        liftChain = hardwareMap.get(DcMotorEx.class, "liftChain");
+        liftChain.setDirection(Constants.liftChainDirection);
+        elbow = hardwareMap.get(DcMotorEx.class, "elbow");
+        elbow.setDirection(Constants.elbowDirection);
+
+        // if we get the numbers then open up
+        if (Constants.liftHover == null || Constants.wristGrab == null || Constants.fingersOpen == null) {
+            setModeDirectional();
+        } else {
+            setModeManual();
+            foot.setTargetPosition(0);
+            liftRope.setTargetPosition(Constants.liftHover);
+            liftChain.setTargetPosition((int) Math.round(Constants.liftHover * 1.01));
+            elbow.setTargetPosition(0);
+            foot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            liftRope.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            liftChain.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            foot.setPower(1);
+            liftRope.setPower(1);
+            liftChain.setPower(1);
+            elbow.setPower(1);
+        }
+
+        while (opModeIsActive()) {
+
+//------------------------------------------------------------------- Manual
+            if (currentMode == Mode.DIRECTIONAL) {
+
+                if (position == 1) {
+                    telemetry.addData(selector + "foot: ", Constants.footDirection);
+                    telemetry.addData(spacer + "liftRope: ", Constants.liftRopeDirection);
+                    telemetry.addData(spacer + "liftChain: ", Constants.liftChainDirection);
+                    telemetry.addData(spacer + "elbow: ", Constants.elbowDirection);
+                } else if (position == 2) {
+                    telemetry.addData(spacer + "foot: ", Constants.footDirection);
+                    telemetry.addData(selector + "liftRope: ", Constants.liftRopeDirection);
+                    telemetry.addData(spacer + "liftChain: ", Constants.liftChainDirection);
+                    telemetry.addData(spacer + "elbow: ", Constants.elbowDirection);
+                } else if (position == 3) {
+                    telemetry.addData(spacer + "foot: ", Constants.footDirection);
+                    telemetry.addData(spacer + "liftRope: ", Constants.liftRopeDirection);
+                    telemetry.addData(selector + "liftChain: ", Constants.liftChainDirection);
+                    telemetry.addData(spacer + "elbow: ", Constants.elbowDirection);
+                } else if (position == 4) {
+                    telemetry.addData(spacer + "foot: ", Constants.footDirection);
+                    telemetry.addData(spacer + "liftRope: ", Constants.liftRopeDirection);
+                    telemetry.addData(spacer + "liftChain: ", Constants.liftChainDirection);
+                    telemetry.addData(selector + "elbow: ", Constants.elbowDirection);
+                }
+
+                if (!last.dpad_up && gamepad1.dpad_up) {
+                    position++;
+                    if (position > 4) {
+                        position = 1;
+                    }
+                }
+                if (!last.dpad_down && gamepad1.dpad_down) {
+                    position++;
+                    if (position < 1) {
+                        position = 4;
+
+                    }
+                }
+            }
+
+//------------------------------------------------------------------- Manual
+            if (currentMode == Mode.MANUAL) {
+                if (position == 1) {
+                    telemetry.addData(selector + "foot: ", foot.getCurrentPosition());
+                    telemetry.addData(spacer + "lift: ", liftRope.getCurrentPosition());
+                    telemetry.addData(spacer + "elbow: ", elbow.getCurrentPosition());
+                } else if (position == 2) {
+                    telemetry.addData(spacer + "foot: ", foot.getCurrentPosition());
+                    telemetry.addData(selector + "lift: ", liftRope.getCurrentPosition());
+                    telemetry.addData(spacer + "elbow: ", elbow.getCurrentPosition());
+                } else if (position == 3) {
+                    telemetry.addData(spacer + "foot: ", foot.getCurrentPosition());
+                    telemetry.addData(spacer + "lift: ", liftRope.getCurrentPosition());
+                    telemetry.addData(selector + "elbow: ", elbow.getCurrentPosition());
+                }
+
+                if (!last.back && gamepad1.back) {
+                    setModeFreezeMotor();
+                    break;
+                }
+                if (!last.dpad_up && gamepad1.dpad_up) {
+                    position++;
+                    if (position > 3) {
+                        position = 1;
+                    }
+                }
+                if (!last.dpad_down && gamepad1.dpad_down) {
+                    position++;
+                    if (position < 1) {
+                        position = 3;
+                    }
+                }
+
+            }
+
+            last = gamepad1;
+            telemetry.update();
+        }
+
+    }
+
+    void setModeManual() {
+        position = 1;
+        foot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftRope.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftChain.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        foot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftRope.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftChain.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        elbow.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        foot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftRope.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftChain.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        foot.setTargetPosition(foot.getCurrentPosition());
+        liftRope.setTargetPosition(liftRope.getCurrentPosition());
+        liftChain.setTargetPosition(liftChain.getCurrentPosition());
+        elbow.setTargetPosition(elbow.getCurrentPosition());
+        foot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftRope.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftChain.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        foot.setPower(1);
+        liftRope.setPower(1);
+        liftChain.setPower(1);
+        elbow.setPower(1);
+        currentMode = Mode.MANUAL;
+    }
+
+    void setModeFreezeMotor() {
+        position = 1;
+        foot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftRope.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftChain.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        foot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftRope.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftChain.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        elbow.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        foot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        liftRope.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        liftChain.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        foot.setTargetPosition(foot.getCurrentPosition());
+        liftRope.setTargetPosition(liftRope.getCurrentPosition());
+        liftChain.setTargetPosition(liftChain.getCurrentPosition());
+        elbow.setTargetPosition(elbow.getCurrentPosition());
+        foot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftRope.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftChain.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        foot.setPower(1);
+        liftRope.setPower(1);
+        liftChain.setPower(1);
+        elbow.setPower(1);
+        currentMode = Mode.FREEZE_MOTOR;
+    }
+
+    void setModeDirectional() {
+        position = 1;
+        foot.setPower(0);
+        liftRope.setPower(0);
+        liftChain.setPower(0);
+        elbow.setPower(0);
+        foot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftRope.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftChain.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        foot.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftRope.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftChain.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        elbow.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        currentMode = Mode.DIRECTIONAL;
+    }
 }
